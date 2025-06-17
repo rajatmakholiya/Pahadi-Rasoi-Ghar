6630e6200c6a62fc769f6b1a5c9c93a7a0ae61e9
venom96 <rajatmakholiya07@gmail.com>
2025-06-17 17:40:15 +0530

    Initial Commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased text-black dark:text-white transition-colors duration-300`}
       >
         {children}
       </body>
